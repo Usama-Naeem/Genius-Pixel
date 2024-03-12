cd398d552a336402e2ee0cebb274868b0a1bcdea
Usama Naeem <usamamanghat917@gmail.com>
2024-03-12 15:57:26 +0500

    configure clerk webhook

--- lib/database/mongoose.ts
+++ lib/database/mongoose.ts
@@ -7,13 +7,6 @@ interface MongooseConnection {
   promise: Promise<Mongoose> | null;
 }
 
-// In express applications, we connect our application with db only once. But in next js, we have to connect with db
-// on each server action or api request. because next js runs in serverless environment. Serverless functions are stateless means they start up
-// to handel a request and shut down right after without maintaining a continuous connection with db. this approach ensures that each req handeled
-// independently to ensure scalability and reliability as no need to manage persistance connection acrooss many instances which works well with scaleable nature
-// of next js but doing that without any optimization means to many mongodb connections performs on server side.
-
-// here for optimization, we cache the mongoose connection.
 let cached: MongooseConnection = (global as any).mongoose;
 
 if (!cached) {
@@ -31,7 +24,7 @@ export const connectToDatabase = async () => {
   cached.promise =
     cached.promise ||
     mongoose.connect(MONGODB_URL, {
-      dbName: "geniuspixel",
+      dbName: "imaginify",
       bufferCommands: false,
     });
 
@@ -39,3 +32,11 @@ export const connectToDatabase = async () => {
 
   return cached.conn;
 };
+
+// In express applications, we connect our application with db only once. But in next js, we have to connect with db
+// on each server action or api request. because next js runs in serverless environment. Serverless functions are stateless means they start up
+// to handel a request and shut down right after without maintaining a continuous connection with db. this approach ensures that each req handeled
+// independently to ensure scalability and reliability as no need to manage persistance connection acrooss many instances which works well with scaleable nature
+// of next js but doing that without any optimization means to many mongodb connections performs on server side.
+
+// here for optimization, we cache the mongoose connection.
